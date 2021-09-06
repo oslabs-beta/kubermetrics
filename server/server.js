6cbc1785f37ad19c4a3d6fe7ef8265d35bb03851
dominicd17 <dominicd17@gmail.com>
2021-09-05 21:33:17 -0700

    npm packages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const client = require('prom-client');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+client.collectDefaultMetrics();
 
 
 app.use("/build", express.static(path.join(__dirname, "../build")));
@@ -9,4 +16,11 @@ app.get("/", (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, "../index.html"));
 });
 
-app.listen(3000);
+app.get('http://localhost:30000/getMetrics', async (req, res) => {
+  console.log('Scraped');
+  console.log(await client.register.getMetricsAsJSON())
+  // console.log(await client.register.getMetricsAsJSON())
+  //res.send(await client.register.getMetricsAsJSON());
+});
+
+app.listen(3080);
