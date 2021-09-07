c7fd317a514edd1ebff04efdd1f0e7dd6cf3852d
dominicd17 <dominicd17@gmail.com>
2021-09-07 12:20:28 -0700

    k8s Yaml file setup

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const client = require('prom-client');
 const cors = require('cors');
+const axios = require('axios');
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +25,9 @@ app.get('http://localhost:30000/getMetrics', async (req, res) => {
   //res.send(await client.register.getMetricsAsJSON());
 });
 
-app.listen(3080, () => {
-  console.log('listening on port 3080')
+app.listen(3080, async () => {
+  await axios.get('http://prometheus-service.monitoring.svc:8080/api/v1/query?query=container_processes&g0.tab=1&g0.stacked=0&g0.show_exemplars=0&g0.range_input=1h')
+  .then(res => console.log(res.data.data.result))
+  .catch(err => console.log('error in axios'));
+  console.log('listening on port 3080');
 });
