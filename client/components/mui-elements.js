68644c14420571356e67349ec5c9890621de86bf
dominicd17 <dominicd17@gmail.com>
2021-09-13 14:23:46 -0700

    Added new custom color palette theme for mui

--- client/components/mui-elements.js
+++ client/components/mui-elements.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
 import { styled } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
+
+export const darkTheme = createTheme({"palette":{"common":{"black":"#000","white":"#fff"},"background":{"paper":"rgba(26, 30, 33, 1)","default":"#fafafa"},"primary":{"light":"rgba(149, 106, 239, 1)","main":"rgba(113, 53, 240, 1)","dark":"rgba(91, 20, 240, 1)","contrastText":"#fff"},"secondary":{"light":"rgba(239, 84, 149, 1)","main":"rgba(245, 6, 107, 1)","dark":"rgba(195, 0, 82, 1)","contrastText":"#fff"},"error":{"light":"#e57373","main":"#f44336","dark":"#d32f2f","contrastText":"#fff"},"text":{"primary":"rgba(0, 0, 0, 0.87)","secondary":"rgba(255, 255, 255, 0.54)","disabled":"rgba(255, 255, 255, 0.38)","hint":"rgba(255, 255, 255, 0.38)"}}})
 
 export const PodElement = styled(Card)({
   display: 'flex',
+  width: '200px',
+  height: '200px',
   color: 'white',
   backgroundColor: 'rgb(85, 85, 85)',
   boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
@@ -14,6 +20,10 @@ export const PodElement = styled(Card)({
   clipPath:
   'circle(40%)',
 });
+
+export const PurpleButton = styled(Button)({
+  background: '#7135f0'
+})
 
 
 // clipPath:
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,10 @@ import { connect } from 'react-redux';
 // import actions from action creators file
 import * as actions from '../actions/actions';
 import HomePage from '../pages/home/index.js'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+
+const darkTheme = createTheme({"palette":{"common":{"black":"#000","white":"#fff"},"background":{"paper":"rgba(26, 30, 33, 1)","default":"#fafafa"},"primary":{"light":"rgba(149, 106, 239, 1)","main":"rgba(113, 53, 240, 1)","dark":"rgba(91, 20, 240, 1)","contrastText":"#fff"},"secondary":{"light":"rgba(239, 84, 149, 1)","main":"rgba(245, 6, 107, 1)","dark":"rgba(195, 0, 82, 1)","contrastText":"#fff"},"error":{"light":"#e57373","main":"#f44336","dark":"#d32f2f","contrastText":"#fff"},"text":{"primary":"rgba(0, 0, 0, 0.87)","secondary":"rgba(255, 255, 255, 0.54)","disabled":"rgba(255, 255, 255, 0.38)","hint":"rgba(255, 255, 255, 0.38)"}}})
+
 
 const mapStateToProps = state => ({
   pods: state.pods.pods
@@ -29,12 +33,14 @@ class App extends React.Component {
     console.log(this.props)
     
     return (
-      <div>
-        <HomePage state={{...this.props}}/>
-        <button onClick={this.props.click}></button>
-        <button onClick={this.props.click2}></button>
-        <button onClick={this.props.click3}></button>
-      </div>
+      <ThemeProvider theme={darkTheme}>
+        <div>
+          <HomePage state={{...this.props}}/>
+          <button onClick={this.props.click}></button>
+          <button onClick={this.props.click2}></button>
+          <button onClick={this.props.click3}></button>
+        </div>
+      </ThemeProvider>
     )
   }
 }
