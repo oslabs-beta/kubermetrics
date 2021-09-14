c36fc108c611aaebf8f7e1e266569dbb26167aab
dominicd17 <dominicd17@gmail.com>
2021-09-13 17:35:11 -0700

    Dark theme implemented

--- client/components/mui-elements.js
+++ client/components/mui-elements.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import { styled } from '@material-ui/core/styles';
-import { createTheme } from '@material-ui/core/styles';
 
-export const darkTheme = createTheme({"palette":{"common":{"black":"#000","white":"#fff"},"background":{"paper":"rgba(26, 30, 33, 1)","default":"#fafafa"},"primary":{"light":"rgba(149, 106, 239, 1)","main":"rgba(113, 53, 240, 1)","dark":"rgba(91, 20, 240, 1)","contrastText":"#fff"},"secondary":{"light":"rgba(239, 84, 149, 1)","main":"rgba(245, 6, 107, 1)","dark":"rgba(195, 0, 82, 1)","contrastText":"#fff"},"error":{"light":"#e57373","main":"#f44336","dark":"#d32f2f","contrastText":"#fff"},"text":{"primary":"rgba(0, 0, 0, 0.87)","secondary":"rgba(255, 255, 255, 0.54)","disabled":"rgba(255, 255, 255, 0.38)","hint":"rgba(255, 255, 255, 0.38)"}}})
 
 export const PodElement = styled(Card)({
   display: 'flex',
@@ -20,11 +18,6 @@ export const PodElement = styled(Card)({
   clipPath:
   'circle(40%)',
 });
-
-export const PurpleButton = styled(Button)({
-  background: '#7135f0'
-})
-
 
 // clipPath:
 //   'polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)'
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,59 +9,38 @@ import AlertsPage from '../pages/alerts/index.js'
 import TerminalPage from '../pages/terminal/index.js'
 import NavRoutes from '../components/NavRoutes/index.js';
 import '../styles.css';
-
-// import NavBar from '../components/navbar/index.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Sidebar from '../components/sidebar/Sidebar.js'
+import Sidebar from '../components/sidebar/Sidebar.js';
+import { createTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
-// import {
-//   BrowserRouter, // root Component which renders the children routes
-//   Route, // individual child components responsible for rendering the UI based on user's input location matching the defined path in <Route>
-//   Link, // used to navigate amongst the internal routes in our router app, think anchor tags
-// } from 'react-router-dom'
-
-
-
-// class App extends React.Component {
-//   render () {
-//     console.log(this.props)
-
-//     return (
-//       <div>
-//         <HomePage state={{...this.props}}/>
-//         <button onClick={this.props.click}></button>
-//         <button onClick={this.props.click4}></button>
-//         <button onClick={this.props.click2}></button>
-//         <button onClick={this.props.click3}></button>
-//       </div>
-//     )
-//   }
-// }
+export const darkTheme = createTheme({"palette":{"common":{"black":"#000","white":"rgba(255, 255, 255, 1)"},"background":{"paper":"rgba(26, 30, 33, 1)","default":"rgba(255, 255, 255, 1)"},"primary":{"light":"rgba(45, 53, 55, 1)","main":"rgba(17, 22, 23, 1)","dark":"rgba(13, 15, 16, 1)","contrastText":"#fff"},"secondary":{"light":"rgba(143, 96, 240, 1)","main":"rgba(113, 53, 240, 1)","dark":"rgba(96, 29, 239, 1)","contrastText":"#fff"},"error":{"light":"#e57373","main":"rgba(208, 2, 27, 1)","dark":"#d32f2f","contrastText":"#fff"},"text":{"primary":"rgba(0, 0, 0, 0.87)","secondary":"rgba(255, 255, 255, 0.54)","disabled":"rgba(255, 255, 255, 0.38)","hint":"rgba(255, 255, 255, 0.38)"}}})
 
 
 
 function App() {
   return (
     <>
-     <Router>
-      <Sidebar />
-      <Switch>
-        <Route path='/' exact component={HomePage} />
-        <Route path='/metrics' exact component={MetricsPage} />
-        <Route path='/alerts' exact component={AlertsPage} />
-        <Route path='/terminal' exact component={TerminalPage} />
+    <MuiThemeProvider theme={darkTheme}>
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/metrics' exact component={MetricsPage} />
+          <Route path='/alerts' exact component={AlertsPage} />
+          <Route path='/terminal' exact component={TerminalPage} />
 
-      </Switch>
+        </Switch>
 
-      </Router>
-      {/* <Switch>
-        {NavRoutes.map((route) => (
-          <Route exact path={route.path} key={route.path}>
-            <route.component />
-          </Route>
-        ))}
-      </Switch> */}
-    </>
+        </Router>
+        {/* <Switch>
+          {NavRoutes.map((route) => (
+            <Route exact path={route.path} key={route.path}>
+              <route.component />
+            </Route>
+          ))}
+        </Switch> */}
+    </MuiThemeProvider>
+      </>
   );
 }
 
