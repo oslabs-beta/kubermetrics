d7b62132be2234e70d47929926902ddf941e988e
dominicd17 <dominicd17@gmail.com>
2021-09-18 11:58:01 -0700

    ZingChart Installation

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ WORKDIR /app
 COPY package.json ./
 
 RUN npm install
+RUN npm install zingchart-react --legacy-peer-deps
 COPY ./ ./
 
 CMD ["npm", "run", "dev"]
--- client/pages/metrics/index.js
+++ client/pages/metrics/index.js
@@ -1,18 +1,16 @@
 import { useEffect } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import React from 'react';
 import { useTheme } from '@material-ui/core/styles';
-// import PodsList from '../../components/Pods/PodsList';
-// import Navbar from '../../components/navbar/navbar'
+import Graph from '../../components/Charts/Testgraph'
 
 
 
 const MetricsPage = (props) => {
   return (
     <div className='MetricsPage'>
-      <p> Hello there </p>
+      <Graph></Graph>
     </div>
   )
 }
