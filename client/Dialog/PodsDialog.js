a8b2c359acffc10984328f7374ff41278143e4b2
dominicd17 <dominicd17@gmail.com>
2021-09-18 18:53:56 -0700

    Pod Dialog Box Table

--- client/Dialog/PodsDialog.js
+++ client/Dialog/PodsDialog.js
@@ -1,57 +1,25 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import { styled } from '@material-ui/core/styles';
+import {  } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/paper';
 
 const customStyle = {
   color: 'white',
   margin: '5px',
   padding: '5px',
 }
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuDialogContent-root': {
-    padding: theme.spacing(2),
-  },
-  '& .MuDialogActions-root': {
-    padding: theme.spacing(1),
-  },
-}));
-
-const BootstrapDialogTitle = (props) => {
-  const { children, onClose, ...other } = props;
-
-  return (
-    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-      {children}
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            borderRadius: '10px',
-            color: 'red'
-          }}
-        >
-        </IconButton>
-      ) : null}
-    </DialogTitle>
-  );
-};
-
-BootstrapDialogTitle.propTypes = {
-  children: PropTypes.node,
-  onClose: PropTypes.func.isRequired,
-};
 
 export default function CustomizedDialogs() {
   const [open, setOpen] = React.useState(false);
@@ -68,36 +36,61 @@ export default function CustomizedDialogs() {
       <Button variant="contained" size='small' color='secondary' onClick={handleClickOpen}>
         More Info
       </Button>
-      <BootstrapDialog
+      <Dialog
+        style={customStyle}
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        <BootstrapDialogTitle id="customized-dialog-title" style={customStyle} onClose={handleClose}>
+        <DialogTitle id="customized-dialog-title" style={customStyle} onClose={handleClose}>
           Pod Info
-        </BootstrapDialogTitle>
+        </DialogTitle>
         <DialogContent dividers>
-          <Typography gutterBottom>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Dessert (100g serving)</TableCell>
+                  <TableCell align="right">Calories</TableCell>
+                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                  <TableRow >
+                    <TableCell component="th" scope="row">
+                      hello
+                    </TableCell>
+                    <TableCell align="right">{1}</TableCell>
+                    <TableCell align="right">{2}</TableCell>
+                    <TableCell align="right">{3}</TableCell>
+                    <TableCell align="right">{4}</TableCell>
+                  </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {/* <Typography style={customStyle} gutterBottom>
             Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
             dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
             consectetur ac, vestibulum at eros.
           </Typography>
-          <Typography gutterBottom>
+          <Typography style={customStyle} gutterBottom>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
             Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
           </Typography>
-          <Typography gutterBottom>
+          <Typography style={customStyle} gutterBottom>
             Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
             magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
             ullamcorper nulla non metus auctor fringilla.
-          </Typography>
+          </Typography> */}
         </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={handleClose}>
             Close
           </Button>
         </DialogActions>
-      </BootstrapDialog>
+      </Dialog>
     </div>
   );
 }
