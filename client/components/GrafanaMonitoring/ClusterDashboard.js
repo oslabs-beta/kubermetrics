b3c05f6942bd61ba85a5281118af8471d181ca35
dominicd17 <dominicd17@gmail.com>
2021-09-21 08:19:38 -0700

    package json

--- client/components/GrafanaMonitoring/ClusterDashboard.js
+++ client/components/GrafanaMonitoring/ClusterDashboard.js
@@ -10,7 +10,7 @@ class ClusterDashboard extends React.Component {
             <iframe
             width="100%"
             height="1000px"
-            src={"http://localhost:3000/d/U_gA5P2mz/kubernetes-cluster-monitoring?orgId=1&refresh=10s&from=1632146668687&to=1632147568687"}
+            src={"https://localhost:3000/"}
             />
       )
   }
